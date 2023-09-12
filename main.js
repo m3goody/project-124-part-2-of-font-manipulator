3dea0c25bac4953a514fdf03e834100ffbbaee69
m3goody <116851784+m3goody@users.noreply.github.com>
2023-09-12 19:49:39 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(550,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function modelLoaded(){
+    console.log('PoseNet Has initialized');
+}
+function draw(){
+    background('#868685');
+}
